9061479b96861a8b68a1eb4661d7c0c66ef77051
Pusher Robot <jack@semantic-ui.com>
2015-03-27 12:31:52 -0400

    Updated component to version 1.11.6

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-segment',
   summary : 'Semantic UI - Segment: Single component release',
-  version : '1.11.5',
+  version : '1.11.6',
   git     : 'git://github.com/Semantic-Org/UI-Segment.git',
 });
 
