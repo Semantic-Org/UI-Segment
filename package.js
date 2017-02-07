ae3127b8b0279e1b34223147e2d2b2360e0593a2
Jack Lukic <jack@semantic-ui.com>
2017-02-07 10:58:15 -0500

    Updated component to version 2.2.7

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-segment',
   summary : 'Semantic UI - Segment: Single component release',
-  version : '2.2.6',
+  version : '2.2.7',
   git     : 'git://github.com/Semantic-Org/UI-Segment.git',
 });
 
